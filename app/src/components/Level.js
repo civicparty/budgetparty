871ce049281fbbcc26cc6709105b253a065d5eee
Mateo Clarke <code@mateoclarke.com>
2017-07-28 10:01:15 -0500

    Update link in Level and calculation

--- app/src/components/Level.js
+++ app/src/components/Level.js
@@ -46,7 +46,7 @@ export default class Level extends Component {
               <Link to={`/level/${level.index}/choices`}
                 className="Level__next-button"
               >
-                {(level.index + 1) < partyLevels.length ? 'View Choices' : 'Review Final Budget'}
+                {(level.index) < partyLevels.length ? 'View Choices' : 'Review Final Budget'}
               </Link>
           }
 
--- app/src/reducers/calculations.js
+++ app/src/reducers/calculations.js
@@ -9,43 +9,44 @@ function calculations(state = [], action = {}) {
 
       return Object.assign({}, state, { budgetAmount: amount })
     case 'UPDATE_COSTS_AMOUNT':
-      console.log('UPDATE_COSTS_AMOUNT', action)
       const { market, mode, guideway, serviceTimes } = action.choices
 
-      let routeDistance = (market && market.distance) || 1
-      let capacityPerVehicle = (mode && mode.capacityPerVehicle) || 1
-      let capitalCostPerMile = (guideway && guideway.capitalCostPerMile) || 1
-      let costPerRevHour = (guideway && guideway.costPerRevHour) || 1
-      let maintenanceCosts = (guideway && guideway.maintenanceCosts) || 1
+      const routeDistance = (market && market.distance) || 1
+      const capacityPerVehicle = (mode && mode.capacityPerVehicle) || 1
+      const capitalCostPerMile = (guideway && guideway.capitalCostPerMile) || 1
+      const costPerRevHour = (guideway && guideway.costPerRevHour) || 1
+      const maintenanceCost = (guideway && guideway.maintenanceCosts) || 1
 
       // TODO, get theses numbers dynamically
       const _vehicleCount = 5     // Need a default value for each mode type
-      const _costPerHour = 10
-      const _revenueMiles = 100
+      // const _costPerHour = 10
+      // const _revenueMiles = 100
 
       const vehicleCost = mode ? _vehicleCount * mode.capitalCostPerVehicle : 0
-      const guidewayCost = market && guideway ? market && market.distance * guideway.capitalCostPerMile : 0
-      const maintenanceCost = guideway ? guideway.maintenanceCosts : 0
+      const guidewayCost = market && guideway ? routeDistance * capitalCostPerMile : 0
       const serviceTimeWeeklySum = () => {
         if (serviceTimes) {
           return serviceTimes.reduce((sum, item) => {
-            return sum + item.hours
+            if (item.checked) {
+              return sum + item.hours
+            } else {
+              return sum
+            }
           }, 0)
         } else {
           return 1
         }
       }
       const revenueHours = serviceTimeWeeklySum() * 52
-      const revenueMiles = _revenueMiles
       const operatingCost = serviceTimes ?
         _vehicleCount * revenueHours * costPerRevHour : 0
       const capacity = _vehicleCount * capacityPerVehicle
 
       const newCalculations = {
-        vehicleCost: vehicleCost,
-        guidewayCost: guidewayCost,
-        maintenanceCost: maintenanceCost,
-        operatingCost: operatingCost,
+        vehicleCost,
+        guidewayCost,
+        maintenanceCost,
+        operatingCost,
         totalCosts: vehicleCost + guidewayCost + maintenanceCost + operatingCost,
       }
 
