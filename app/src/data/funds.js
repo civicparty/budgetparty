257cce2049870283e8acf49e23a87bf82df1cc61
Mateo Clarke <code@mateoclarke.com>
2017-08-21 01:42:16 -0500

    delete unneeded files

--- app/src/data/funds.js
+++
@@ -1,9 +0,0 @@
-const funds = {
-  generalFund: 969200000, // TOTAL FUNDS AVAILABLE
-  generalFund2016: 911000000,
-  lastYearServicesSum: 939221442, // need to doublecheck and update this number in line with the sum we use for departments
-  sumOfServiceSpending: null,
-  generalFundsRemaining: null,
-}
-
-export default funds;
--- app/src/data/selections.js
+++
@@ -1,3 +0,0 @@
-const selections = [];
-
-export default selections;
