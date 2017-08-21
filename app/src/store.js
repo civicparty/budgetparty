6b7052182919ce93d502849edceea8c313e59e64
Mateo Clarke <code@mateoclarke.com>
2017-08-21 01:42:30 -0500

    add default values

--- app/src/store.js
+++ app/src/store.js
@@ -1,8 +1,38 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers/index';
 
+const serviceTimeDefaultState = {
+  serviceTimes: {
+    hasDefaultValues: true,
+    1: {
+      id: 1,
+      frequencyId: 0,
+      hoursPerWeekday: 6,
+      frequencyValue: 15,
+    },
+    2: {
+      id: 2,
+      frequencyId: 0,
+      hoursPerWeekday: 10,
+      frequencyValue: null,
+    },
+    3: {
+      id: 3,
+      frequencyId: 0,
+      hours: 28,
+      frequencyValue: null,
+    },
+    4: {
+      id: 4,
+      frequencyId: 0,
+      hours: 12,
+      frequencyValue: null,
+    },
+  },
+};
+
 const defaultState = {
-  // TODO: Update
+  choices: serviceTimeDefaultState,
 }
 
 const store = createStore(
